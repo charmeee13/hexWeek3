ea9ed56e1abfe58cce1e0ac060b862968b9263ac
charmeee13 <98146273+charmeee13@users.noreply.github.com>
2022-08-04 19:51:10 +0800

    Update 2022-08-04T11:51:08.434Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,7 +1,25 @@
 "use strict";
 
-console.log('Hello!');
-$(document).ready(function () {
-  console.log('HesSchool Hello!');
+// $(document).ready(function (e) {
+//   $('.menu').on('click', function (e) {
+//     console.log('sucess');
+//     e.preventDefault();
+//     $('.header-list').toggleClass('overlay');
+//   })
+//   console.log('fail');
+// });
+$(function () {
+  $(".btn_open").on("click", function (e) {
+    $(".overlay").fadeIn();
+  });
+  $('.btn_open').on('click', function (e) {
+    $('.menu').addClass('show');
+  });
+  $(".btn_close").on("click", function (e) {
+    $(".overlay").fadeOut();
+  });
+  $('.btn_close').on('click', function (e) {
+    $('.menu').removeClass('show');
+  });
 });
 //# sourceMappingURL=all.js.map
