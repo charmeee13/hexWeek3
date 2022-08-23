ccea0de19afae26e217836da90c0c16344b925e3
charmeee13 <98146273+charmeee13@users.noreply.github.com>
2022-08-23 15:30:09 +0800

    lv1 update

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,5 +1,28 @@
-console.log('Hello!');
 
-$(document).ready(() => {
-  console.log('HesSchool Hello!');
+
+// $(document).ready(function (e) {
+//   $('.menu').on('click', function (e) {
+//     console.log('sucess');
+//     e.preventDefault();
+//     $('.header-list').toggleClass('overlay');
+//   })
+//   console.log('fail');
+// });
+
+$(function () {
+  $(".btn_open").on("click", function (e) {
+    $(".overlay").fadeIn();
+  });
+
+  $('.btn_open').on('click', function (e) {
+    $('.menu').addClass('show');
+  })
+
+  $(".btn_close").on("click", function (e) {
+    $(".overlay").fadeOut();
+  });
+
+  $('.btn_close').on('click', function (e) {
+    $('.menu').removeClass('show');
+  })
 });
